4d25c5a2c206851ba95dd2fbe85e872d29d4ee3b
Damien Cosset <dcossetfreelance@gmail.com>
2017-02-10 15:53:02 +0100

    Added some commands and insert docs

---
+++ documentation/insert.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+exports.insert = function(){
+    console.log('To enter the insertion mode: press the letter i .\n');
+    console.log('You should see --INSERT-- appear at the bottom');
+}
