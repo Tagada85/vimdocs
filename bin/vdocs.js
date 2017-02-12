749c3ac876b9e577a6c713aef509baed8bd8faa3
Damien Cosset <dcossetfreelance@gmail.com>
2017-02-12 20:35:12 +0100

    Added usage key

--- bin/vdocs.js
+++ bin/vdocs.js
@@ -1,38 +1,32 @@
 #!/usr/bin/env node
 const shell = require('shelljs');
 const args = process.argv.slice(2);
-const insert = require('../documentation/insert').insert;
-
-const usage = [
-    '',
-    ' Usage: vdocs <actionName>',
-    '',
-    ' -h, --help     Display usage information',
-    ' -V, --version  Output the current version',
-    ' --insert   Shows how to insert text in vim',
-    ' --keys     Shows a list of shortcuts supported by vdocs'
-].join('\n');
+const Insert = require('../documentation/insert');
+const Usage = require('../documentation/usage');
+const Movement = require('../documentation/movements');
+const ansi = require('ansi');
+const cursor = ansi(process.stdout);
 
 let arg = args.shift();
 switch(arg) {
     case '-h':
     case '--help':
-        console.log(usage);
+        Usage();
         process.exit();
         break;
-    case 'cut':
-        console.log('Cut a line with v (visual mode) and d');
+    case '-insert':
+	Insert();
         process.exit();
         break;
-    case 'insert':
-        insert();
-        process.exit();
-        break;
+    case '-movement':
+	Movement();
+	process.exit();
+	break;
     case '':
         console.log('You need to enter a command');
         process.exit();
         break;
-    case default:
+    default:
         console.log('This command is not supported by vdocs');
         console.log('Use vdocs --keys to see a list of commands')
         process.exit();
